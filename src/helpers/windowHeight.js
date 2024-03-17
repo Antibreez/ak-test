a2b63e6a7b986410c04533b488e7cdd6d617e920
Anton Kiyan <kiyananton@gmail.com>
2024-03-18 00:45:22 +0300

    edits

--- src/helpers/windowHeight.js
+++ src/helpers/windowHeight.js
@@ -1,9 +1,11 @@
+import { debounce } from "./utils";
+
 export default {
   init: function() {
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
   },
   addListener: function() {
-    window.addEventListener('resize', () => this.init());
+    window.addEventListener('resize', debounce(() => this.init()));
   }
 }
